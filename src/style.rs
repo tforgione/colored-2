cdf52c11ae4d9aa064a2fb660f94d273533376d8
Thomas Wickham <mackwic@gmail.com>
2017-03-05 12:02:06 +0100

    fix codes for reversed and hidden (#17)
    * fix codes for reversed and hidden
    
    *  update ansi term version for compat

--- src/style.rs
+++ src/style.rs
@@ -43,8 +43,8 @@ impl Styles {
             Styles::Italic => "3",
             Styles::Underline => "4",
             Styles::Blink => "5",
-            Styles::Reversed => "6",
-            Styles::Hidden => "7",
+            Styles::Reversed => "7",
+            Styles::Hidden => "8",
         }
     }
 
